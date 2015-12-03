5b92c896e92b3c7bee03f4c0b8cc164288c95fcb
Matt Bierner <mattbierner@gmail.com>
2015-12-02 21:21:56 -0800

    Autoscale turtle drawing.

--- draw_run_turtle.py
+++ draw_run_turtle.py
@@ -5,6 +5,11 @@ from common import *
 from config import *
 from process import *
 
+screen_width = 800
+screen_height = 800
+padding = 20
+zoom = 1
+
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help='Data file path.')
 parser.add_argument('--raw', action='store_true', default=False,
@@ -17,7 +22,33 @@ args = parser.parse_args()
 
 game_runs = load_raw_run(args.file) if args.raw else load_run(args.file)
 
+
+# Compute the bounds
+max_x = 1
+max_y = 1
+
+for run in game_runs:
+    pos = (0, 0)
+    for level in run['levels']:
+        for move in level['events']:
+            if not move.get('action', False):
+                keys = move['keys']
+                mul = move['duration']
+                dx = get_x(keys) * mul
+                dy = get_y(keys) * mul
+                end = (pos[0] + dx, pos[1] + dy)
+                max_x = max(max_x, abs(end[0]))
+                max_y = max(max_y, abs(end[1]))
+                pos = end
+
+x_scale = (screen_width / 2.0 - padding) / max_x
+y_scale = (screen_width / 2.0 - padding) / max_y
+scale = min(x_scale, y_scale) * zoom
+
+
 # Start drawing
+turtle.setup(width = screen_width, height = screen_height)
+
 t = turtle.Turtle()
 
 if not args.animate:
@@ -35,7 +66,7 @@ for run in game_runs:
             else:
                 keys = move['keys']
                 t.width(2 if SHIFT in keys else 1)
-                mul = move['duration'] * 20
+                mul = move['duration'] * scale
                 x = get_x(keys) * mul
                 y = get_y(keys) * mul
                 t.setpos(x + t.xcor(), y + t.ycor())
